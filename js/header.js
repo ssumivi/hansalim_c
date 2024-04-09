8bfd80a68af72f700335440125423caa123b0a5b
suminkim <di05023@gmail.com>
2024-04-09 12:46:01 +0900

    오늘의 물품까지 js 작성 완료.
    다음 섹션 제작 선거일 이후 진행됩니다~

--- js/header.js
+++ js/header.js
@@ -28,7 +28,7 @@ window.addEventListener("load", function () {
   const tgListArr = [menuList, joinList, centerList];
   const tgBtArr = [menuBt, joinBt, centerBt];
   //펼침 목록 모두 닫기
-  this.document.addEventListener("click", function () {
+  document.addEventListener("click", function () {
     tgListArr.forEach(function (item) {
       item.style.display = "none";
     });
@@ -37,23 +37,93 @@ window.addEventListener("load", function () {
       item.classList.remove("active");
     });
   });
+  //목록 전체를 클릭해도 이벤트 전달 막기
+  tgListArr.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      e.stopPropagation();
+    });
+  });
   //same fn 반복
   function listTg(bt, list) {
     //first, dont list show
     list.style.display = "none";
     //click event! fnc
     bt.addEventListener("click", function (e) {
-      e.preventDefault();
+      e.stopPropagation();
+      // 모든 버튼 클래스 초기화
       tgBtArr.forEach(function (item) {
         item.classList.remove("active");
       });
+      // 현재 목록 ID 가져오기
       const nowList = list.getAttribute("id");
+      // 숨길 목록을 필터링
+      const hideArr = tgListArr.filter(function (item) {
+        let id = item.getAttribute("id");
+        // console.log(id);
+        // 현재 목록과 다른 경우만 필터링
+        if (id !== nowList) {
+          return item;
+        }
+      });
+      //새로 저장된 배열의 목록
+      // console.log(hideArr);
+      hideArr.forEach(function (item) {
+        item.style.display = "none";
+      });
+      const css = getComputedStyle(list).display;
+      if (css === "none") {
+        list.style.display = "block";
+        bt.classList.add("active");
+      } else {
+        list.style.display = "none";
+        bt.classList.remove("active");
+      }
     });
   }
-  listToggle(menuBt, menuList);
+  listTg(menuBt, menuList);
   // toggleListArr[0] = menuList
-  listToggle(joinBt, joinList);
+  listTg(joinBt, joinList);
   // toggleListArr[1] = joinList
-  listToggle(centerBt, centerList);
+  listTg(centerBt, centerList);
   // toggleListArr[2] = centerList
+
+  //전체메뉴 펼침 기능
+  const allMenuArea = this.document.querySelector(".all-menu-area");
+  const allMenu = this.document.querySelector(".all-menu");
+  const cateList = this.document.querySelector(".cate-list");
+  //꼬마와땅부터 메뉴창 안 열리게 하기
+  const deliList = this.document.querySelector(".deli-menu");
+  const themeList = this.document.querySelector(".theme-list");
+  let isMenuOpen = false;
+  cateList.addEventListener("mouseleave", function () {
+    if (!isMenuOpen) {
+      allMenu.classList.remove("active");
+    }
+  });
+  // console.log(cateList);
+  //ul cate-list
+  const cateListWrap = this.document.querySelector(".all-menu-cate-wrap");
+  cateList.addEventListener("mouseleave", function () {
+    allMenu.classList.remove("active");
+  });
+  cateList.addEventListener("mouseenter", function () {
+    allMenu.classList.add("active");
+  });
+  cateListWrap.addEventListener("mouseenter", function () {
+    allMenu.classList.add("active");
+  });
+  // 해당 카테고리 내용만 보여주기
+  const cateLists = this.document.querySelectorAll(".cate-list > li");
+  const cateDepth2 = this.document.querySelectorAll(".cate-depth2-list");
+  cateLists.forEach(function (item, idx) {
+    item.addEventListener("mouseenter", function () {
+      cateDepth2.forEach(function (itemSub, idxSub) {
+        // console.log(itemSub);
+        itemSub.style.display = "none";
+        if (idx == idxSub) {
+          itemSub.style.display = "block";
+        }
+      });
+    });
+  });
 });
---
+++ js/script.js
@@ -0,0 +1,151 @@
+window.addEventListener("load", function () {
+  // data.json 을 로딩, 연결
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function (e) {
+    // console.log(req);
+    const req = e.target;
+    if (req.readyState === XMLHttpRequest.DONE) {
+      const str = req.response;
+      //글자로 온 데이터를 객체로 변환
+      //글자가 json 규칙대로 만들어진 문자열
+      //json 의 글자를 객체로 변환해서 사용
+      let obj = JSON.parse(str);
+      // console.log(obj);
+      VISUAL_ARR = obj.visual;
+      TODAY_GOOD = obj.todaygood;
+      //visual을 화면에 배치
+      showVisual();
+      //today goods 화면에 배치
+      showTodayGoods();
+    }
+  };
+  //자료 호출
+  xhttp.open("GET", "data.json");
+  //웹브라우저 기능 실행 요청
+  xhttp.send();
+  //visual slide
+  let VISUAL_ARR;
+  let visualTag = this.document.getElementById("data-visual");
+  //visual 화면 출력
+  function showVisual() {
+    let html = "";
+    VISUAL_ARR.forEach((item) => {
+      const tag = `
+        <div class="swiper-slide">
+        <div class="visual-slide-pg">
+          <a href="${item.link}">
+            <img src="images/${item.pic}" alt="${item.name}">
+          </a>
+        </div>
+      </div>
+        `;
+      //json 변수 가져와서 item 선언
+      html += tag;
+    });
+    visualTag.innerHTML = html;
+
+    //swiper fnc
+    const swVisual = new Swiper(".sw-visual", {
+      loop: true,
+      autoplay: true,
+      speed: 1000,
+      //   disableOnInteraction: false,
+      navigation: {
+        prevEl: ".visual-prev",
+        nextEl: ".visual-next",
+      },
+      pagination: {
+        // 하나하나 넘어가는 것.
+        el: ".visual-pg",
+        type: "fraction", //pagination count
+      },
+    });
+    //slide pause
+    const swVisualPause = document.querySelector(".visual-play");
+    swVisualPause.addEventListener("click", function () {
+      //current active class exist check, fnc
+      // if classList contains active,
+      if (swVisualPause.classList.contains("active")) {
+        swVisualPause.classList.remove("active");
+        swVisual.autoplay.start();
+      } else {
+        swVisualPause.classList.add("active");
+        swVisual.autoplay.stop();
+      }
+    });
+  }
+  //====visual area end=====
+  //today section
+  let TODAY_GOOD;
+  let today_tag = this.document.getElementById("data-today");
+  let today_tag_2 = this.document.getElementById("data-today2");
+  // 가격 표시 함수 정의
+  function priceToString(price) {
+    // 숫자를 문자열로 변환
+    let priceString = price.toString();
+    // 정규 표현식을 사용하여 세 자리마다 쉼표 추가
+    return priceString.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  function showTodayGoods() {
+    let htmlTop = "";
+    let htmlbt = "";
+    const topArr = TODAY_GOOD.filter(function (item, idx) {
+      // console.log(idx);
+      if (idx < 4) {
+        return item;
+      }
+    });
+    topArr.forEach(function (item) {
+      // console.log(item);
+      let tag = `
+      <div class="good-box">
+      <!-- 제품이미지 -->
+      <a href="${item.link}" class="good-img">
+          <img src="images/${item.pic}" alt="${item.name}">
+      </a>
+      <!-- 제품정보 -->
+      <a href="${item.link}" class="good-info">
+          <em>${item.name}</em>(<em>${item.unit}</em>)
+      </a>
+      <!-- 제품가격 -->
+      <a href="${item.link}" class="good-info-price">
+      ${priceToString(item.price)}<em>원</em>
+      </a>
+      <!-- 장바구니 이미지 -->
+      <button class="good-add-cart"></button>
+      </div>
+      `;
+      htmlTop += tag;
+    });
+    //bt today goods
+    const btArr = TODAY_GOOD.filter(function (item, idx) {
+      if (idx > 3) {
+        return item;
+      }
+    });
+    btArr.forEach(function (item) {
+      // console.log(item);
+      let tag = `
+      <div class="good-box">
+      <!-- 제품이미지 -->
+      <a href="${item.link}" class="good-img">
+          <img src="images/${item.pic}" alt="${item.name}">
+      </a>
+      <!-- 제품정보 -->
+      <a href="${item.link}" class="good-info">
+          <em>${item.name}</em>(<em>${item.unit}</em>)
+      </a>
+      <!-- 제품가격 -->
+      <a href="${item.link}" class="good-info-price">
+      ${priceToString(item.price)}<em>원</em>
+      </a>
+      <!-- 장바구니 이미지 -->
+      <button class="good-add-cart"></button>
+      </div>
+      `;
+      htmlbt += tag;
+    });
+    today_tag.innerHTML = htmlTop;
+    today_tag_2.innerHTML = htmlbt;
+  }
+});
